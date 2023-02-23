aed62e7ae738a42f064afd9c308a7269f9162821
Marcus Gibson <marcusgibson@Marcuss-MacBook-Pro.local>
2023-02-22 23:41:00 -0500

    updated hw1

---
+++ PyBank/main.py
@@ -0,0 +1,40 @@
+#This is is the financial analysis for PyBank
+#Source is from budget_Data.csv
+
+budget_data = "Resources/ budget_Data.csv"
+
+#Create a variable called 'date' that holds an integer
+date = 86
+
+#create a variable called 'total' that holds an integer
+total = 22564198
+
+#Create a variable called 'average_change' that holds an integer
+average_change = -8311.11
+
+#Create a variable called 'month_increase' that holds an string
+month_increase = August_16 
+
+#create a variable called 'increase_amount' that holds an integer
+increase_amount = 1862002
+
+#Create a variable called 'month_decrease' that holds an string
+month_decrease = February_14
+
+#Create a variable called 'decrease_amount' that holds an integer
+decrease_amount = -1825558
+
+#print out "Total Months"
+print("The total months" + str('date' )
+
+#print out "combined total "
+print("The combined" + str('total' )
+
+#print out "average of profit and loss"
+print("the average of profit and loss" + str('average_change')
+
+#print out "Greatest increase in profit(dates and amounts)"
+print("The greatest increase in profits + str('increase_amount') + occured on + month_increase")
+
+#print out "Greatest decrease in profits(dates and amounts)"
+print("The greatest decrease in profits + str('decrease_amount') + occured on + month_increase")
